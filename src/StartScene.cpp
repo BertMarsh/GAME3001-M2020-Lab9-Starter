b2368c42d589059c99890db46fd2454f4fb416e5
gildobro <gdobrovinsky@gmail.com>
2020-07-22 23:32:17 -0400

    Added my name and student ID.

--- src/StartScene.cpp
+++ src/StartScene.cpp
@@ -60,18 +60,24 @@ void StartScene::handleEvents()
 void StartScene::start()
 {
 	const SDL_Color orange = { 196, 105, 0, 255 };
-
+	const SDL_Color purple = { 255, 0, 255, 0 };
 	m_pStartLabel = new Label("START SCENE", "Consolas", 80, orange, glm::vec2(400.0f, 40.0f));
 	m_pStartLabel->setParent(this);
 	addChild(m_pStartLabel);
 
 	m_pInstructionsLabel = new Label("Robert Marshall", "Consolas", 40, orange, glm::vec2(400.0f, 120.0f));
+	m_pInstructionsLabel2 = new Label("Gil Dobrovinsky", "Consolas", 40, purple, glm::vec2(400.0f, 180.0f));
 	m_pInstructionsLabel->setParent(this);
+	m_pInstructionsLabel2->setParent(this);
 	addChild(m_pInstructionsLabel);
+	addChild(m_pInstructionsLabel2);
 
 	m_pInstructionsLabel = new Label("101287661", "Consolas", 40, orange, glm::vec2(410.0f, 150.0f));
+	m_pInstructionsLabel2 = new Label("101304972", "Consolas", 40, purple, glm::vec2(410.0f, 210.0f));
 	m_pInstructionsLabel->setParent(this);
+	m_pInstructionsLabel2->setParent(this);
 	addChild(m_pInstructionsLabel);
+	addChild(m_pInstructionsLabel2);
 
 	// Start Button
 	m_pStartButton = new Button();
